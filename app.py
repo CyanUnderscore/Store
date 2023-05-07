c3ee26cb25171369f9b82255f0e4a30921758e98
CyanUnderscore <106006219+CyanUnderscore@users.noreply.github.com>
2023-05-07 15:19:52 +0200

    on avance toujour

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import csv
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
@@ -10,6 +11,27 @@ def home():
 def contact():
     return render_template('contact.html')
 
+@app.route('/submit_contact', methods=['POST'])
+def submit_contact():
+    # recuperer les informations
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    # mettre dans fichier csv
+    with open('messages.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([name, email, message])
+
+    return redirect(url_for('contact'))
+
+@app.route('/filter_product', methods=['POST'])
+def filter_product():
+    category = request.form['category']
+    product_list = toDico()
+    if category == "":
+        return render_template('products.html', products=product_list)
+    selected_products = [x for x in product_list if x['category'] == category]
+    return render_template('products.html', products=selected_products)
 
 def toDico():
     file = open("database.csv", "r")
@@ -30,8 +52,6 @@ def toDico():
 def products():
     products_list = toDico()
     print(products_list)
-    # transferer info cers html
-
     return render_template('products.html', products=products_list)
 
 @app.route('/product/<id>')
